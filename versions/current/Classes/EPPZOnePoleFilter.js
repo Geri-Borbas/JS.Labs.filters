1872ac5f946c67938609e5adeda5c043c1e23616
Borbás Geri, eppz! <hello@eppz.eu>
2013-07-06 07:00:25 +0200

    Created PHPStorm project, added live demo.

---
+++ versions/current/Classes/EPPZOnePoleFilter.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Created by Borbás Geri on 7/6/13
+ * Copyright (c) 2013 eppz! development, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+var EPPZOnePoleFilter = EPPZLayer.extend
+({
+    color: function()
+    { return 'red'; },
+
+    filter: function()
+    {
+        /*
+        var filter = 0.2;
+        this.filteredSample.x = this.sample.x * filter + this.previousSample.x * (1.0 - filter);
+        this.filteredSample.y = this.sample.y * filter + this.previousSample.y * (1.0 - filter);
+        */
+    }
+
+});
